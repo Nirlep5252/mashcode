e9a6bf38c95a107e3a57a7874450049358de2fd1
Nirlep5252 <nirlep5252@proton.me>
2024-01-08 22:43:31 +0530

    Some more database stuff (idk)

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,14 @@
 import { pgSchema, integer, timestamp } from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 export const dbSchema = pgSchema("mashcode_schema");
 
 export const usersTable = dbSchema.table("users", {
   id: integer("id").primaryKey().notNull(),
   createdAt: timestamp("created_at").notNull(),
+});
+
+export const db = drizzle(postgres(process.env.DB_URL!), {
+  schema: { dbSchema, usersTable },
 });
