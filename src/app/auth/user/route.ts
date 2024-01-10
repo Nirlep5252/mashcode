08993ffdd6cd57fbe51039b232261fc393b64b77
Nirlep5252 <nirlep5252@proton.me>
2024-01-10 11:59:58 +0530

    Add user to database if not already present

--- src/app/auth/user/route.ts
+++ src/app/auth/user/route.ts
@@ -1,3 +1,6 @@
+import { db, users } from "@/lib/db";
+import { eq } from "drizzle-orm";
+
 export async function GET(request: Request) {
   const ghToken = request.headers.get("Cookie")?.split("gh-token=")?.[1];
   if (!ghToken) {
@@ -10,6 +13,11 @@ export async function GET(request: Request) {
   const data = await response.json();
   if (data.message) {
     return new Response(data.message, { status: 400 });
+  }
+  const id = Number.parseInt(data.id);
+  const user = await db.select().from(users).where(eq(users.id, id));
+  if (user.length === 0) {
+    await db.insert(users).values({ id, createdAt: new Date() });
   }
   return new Response(JSON.stringify(data), {
     headers: { "Content-Type": "application/json" },
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,14 +1,12 @@
-import { pgSchema, integer, timestamp } from "drizzle-orm/pg-core";
+import { integer, timestamp, pgTable } from "drizzle-orm/pg-core";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-export const dbSchema = pgSchema("mashcode_schema");
-
-export const usersTable = dbSchema.table("users", {
+export const users = pgTable("users", {
   id: integer("id").primaryKey().notNull(),
   createdAt: timestamp("created_at").notNull(),
 });
 
 export const db = drizzle(postgres(process.env.DB_URL!), {
-  schema: { dbSchema, usersTable },
+  schema: { usersTable: users },
 });
