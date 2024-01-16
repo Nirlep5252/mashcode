c287e6b60ec537defeac00129e7243c915859171
Nirlep5252 <nirlep5252@proton.me>
2024-01-16 10:04:11 +0530

    Refactor logout route to use named export for GET function

--- src/app/logout/route.ts
+++ src/app/logout/route.ts
@@ -1,10 +1,10 @@
-export default function GET() {
-  return {
+export function GET() {
+  // Remove cookie `gh-token` and redirect to home page
+  return new Response(null, {
     headers: {
       "Set-Cookie": "gh-token=; Path=/; HttpOnly; Max-Age=0",
+      Location: "/",
     },
-    body: {
-      success: true,
-    },
-  };
+    status: 302,
+  });
 }
