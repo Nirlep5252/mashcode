d69081ec96055dc7681a36ae709a4096c6870066
Pal_314 <4130.stkabirdin@gmail.com>
2024-02-14 23:14:27 +0530

    Added new router for scraping questions ,updated auth middleware and UI of practice page

--- frontend/src/routes/practice.component.tsx
+++ frontend/src/routes/practice.component.tsx
@@ -1,7 +1,29 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export const component = function Practice() {
   return (
-    <div className="p-2">
-      <h3>Practice</h3>
+    <div className={"w-full h-screen flex items-center justify-around"}>
+      <div className="w - 1/3 flex flex-col items-center justify-center gap-10">
+      <div className="previous-matches w-full flex column">
+        <Card className={"w-[380px] min-h-[400px]"}>
+          <CardHeader>
+            <CardTitle>Unsolved Questions</CardTitle>
+          </CardHeader>
+          <CardContent className={"flex items-center justify-center"}>
+              
+          </CardContent>
+        </Card>
+      </div>
+      <Button size={"lg"} className={"font-bold scale-150 text-lg"}>
+          Practice
+      </Button>
+      </div>
     </div>
   );
 };
