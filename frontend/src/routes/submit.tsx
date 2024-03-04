d5b92111924318f8370de1886a3fb76b62e988ad
Pal_314 <4130.stkabirdin@gmail.com>
2024-03-05 00:26:12 +0530

    Added codemirror to submit page instead of monaco

--- frontend/src/routes/submit.tsx
+++ frontend/src/routes/submit.tsx
@@ -1,45 +1,107 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card.tsx";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button.tsx";
-import { useEffect, useState } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card.tsx";
-import Editor from "@monaco-editor/react";
-import { editor } from "monaco-editor";
+import {
+  githubLight,
+  githubLightInit,
+  githubDark,
+  githubDarkInit,
+} from "@uiw/codemirror-theme-github";
+import { LanguageSupport } from "@codemirror/language";
+import { markdown } from "@codemirror/lang-markdown";
+import { javascript } from "@codemirror/lang-javascript";
+import { cpp } from "@codemirror/lang-cpp";
+import { html } from "@codemirror/lang-html";
+import { json } from "@codemirror/lang-json";
+import { java } from "@codemirror/lang-java";
+import { python } from "@codemirror/lang-python";
 
 export const Route = createFileRoute("/submit")({
   component: Submit,
 });
 
+const EXTENSIONS: { [key: string]: LanguageSupport[] } = {
+  markdown: [markdown()],
+  python: [python()],
+  javascript: [javascript()],
+  typescript: [javascript()],
+  cpp: [cpp()],
+  "c++": [cpp()],
+  html: [html()],
+  json: [json()],
+  java: [java()],
+};
+
+const THEMES: { [key: string]: any } = {
+  githubDark: githubDark,
+  githubLight: githubLight,
+};
+
 function Submit() {
-    const [language, setLanguage] = useState("javascript");
+  const [language, setLanguage] = useState("python");
+  const [theme, setTheme] = useState("githubDark");
+  const [text, setText] = useState("");
   return (
     <div className={"w-full h-screen flex items-center justify-around"}>
-      <div className="w - 1/2 flex flex-col items-center justify-center gap-10">
-        <Card className={"w-[960px] min-h-[500px]"}>
-          <CardHeader>
-            <CardTitle>Submit your solution</CardTitle>
-            <CardContent>
-              <Editor
-                height="70vh"
-                defaultLanguage="javascript"
-                defaultValue="// write your code here"
-                language= {language}
-              />
-              <select
-                className="w-1/3 mt-4"
-                onChange={(e) => setLanguage(e.target.value)}
-                >
-                <option value="javascript">Javascript</option>
-                <option value="python">Python</option>
-                <option value="c">C</option>
-                <option value="cpp">C++</option>
-                </select>
-              <Button size={"sm"} className={"font-bold scale-150"}>
-                Submit
-              </Button>
-            </CardContent>
-          </CardHeader>
-        </Card>
+      <div className="w - 1/3 flex flex-col items-center justify-center gap-10">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant={"ghost"}>
+              {language[0].toUpperCase() + language.slice(1)}
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel inset>Select Language</DropdownMenuLabel>
+            {Object.keys(EXTENSIONS).map((lang) => (
+              <DropdownMenuItem key={lang} onClick={() => setLanguage(lang)}>
+                {lang[0].toUpperCase() + lang.slice(1)}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant={"ghost"}>{theme}</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel inset>Select Theme</DropdownMenuLabel>
+            {Object.keys(THEMES).map((theme) => (
+              <DropdownMenuItem key={theme} onClick={() => setTheme(theme)}>
+                {theme}
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <div className="previous-matches w-full flex column">
+          <CodeMirror
+            value={text}
+            onChange={(newValue) => setText(newValue)}
+            theme={THEMES[theme]}
+            extensions={EXTENSIONS[language]}
+            basicSetup={{ autocompletion: true }}
+            minWidth={"500px"}
+            minHeight={"500px"}
+          />
+        </div>
+        <Button size={"sm"} className={"font-bold scale-150 text-lg"}>
+          Submit
+        </Button>
       </div>
     </div>
-  )
+  );
 }
