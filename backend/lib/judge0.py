0d7d3612c38f1b804ddc14d420fef540b77a3d8e
Pal_314 <4130.stkabirdin@gmail.com>
2024-03-21 00:25:39 +0530

    Added get verdict in judge0

--- backend/lib/judge0.py
+++ backend/lib/judge0.py
@@ -1,21 +1,22 @@
 # languageid, problemid, sourcecode submitted to docker container
 
-import asyncio
 import json
+import time
 
 import aiohttp
 
-problem_details = open("./routers/problem_details.json")
-problem_details_data = json.load(problem_details)
+problem_details_data = json.load(open("routers/problem_details.json"))
 
 
-async def judge0(language_id, problem_id, source_code, url):
+async def get_token(
+    language_id, problem_id, source_code, url, expected_output="Hello World"
+):
     async with aiohttp.ClientSession() as session:
         submission_data = {
             "language_id": language_id,
             "source_code": source_code,
             "stdin": "",
-            "expected_output": "",
+            "expected_output": expected_output,
             "cpu_time_limit": int(
                 float(
                     problem_details_data[str(problem_id)]["problem_time_limit"].strip(
@@ -34,16 +35,41 @@ async def judge0(language_id, problem_id, source_code, url):
             "max_file_size": 1024,
         }
         async with session.post(url, json=submission_data) as response:
-            return await response.json()
+            request_json = await response.json()
+            token = request_json["token"]
+            return {"token": token}
 
 
-token = asyncio.run(
-    judge0(
-        71,
-        1,
-        "print('Hello World')",
-        "http://localhost:2358/submissions",
-    )
-)
+async def get_submission_verdict(
+    url,
+    problem_id,
+    lanuage_id=71,
+    source_code="print('Hello World')",
+    expected_output="Hello World",
+):
+    async with aiohttp.ClientSession() as session:
+        token = await get_token(
+            lanuage_id,
+            problem_id,
+            source_code,
+            url,
+            expected_output=expected_output,
+        )
+        token = token["token"]
+        time.sleep(
+            int(
+                float(
+                    problem_details_data[str(problem_id)]["problem_time_limit"].strip(
+                        "s"
+                    )
+                )
+            )
+            + 5
+        )
+        async with session.get(url + token) as response:
+            request_json = await response.json()
+            return request_json["status"]["description"]
 
-print(token)
+
+# verdict = asyncio.run(get_submission_verdict("http://localhost:2358/submissions/", 1))
+# print(verdict)
